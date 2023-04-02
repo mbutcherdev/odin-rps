0d89ab26136b9c95cedb88499254becf63907cb7
Matthew Butcher <99278651+mbutcherdev@users.noreply.github.com>
2023-04-02 19:30:01 +0100

    Adding GUI to RPS game

--- src/script.js
+++ src/script.js
@@ -1,6 +1,10 @@
 let rounds = 0;
 let playerScore = 0;
 let computerScore = 0;
+const sel_rock = document.querySelector("#selectRock");
+const sel_paper = document.querySelector("#selectPaper");
+const sel_scissors = document.querySelector("#selectScissors");
+let result = "";
 
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
@@ -17,53 +21,77 @@ function getComputerChoice() {
 function playRound(playerChoice, computerChoice) {
   rounds++;
   if (playerChoice === computerChoice) {
-    return "Tie!";
+    return "tie!";
   } else if (playerChoice === "rock") {
     if (computerChoice === "paper") {
       computerScore++;
-      return "You Lose! Paper beats Rock";
+      return "you lose!";
     } else {
       playerScore++;
-      return "You Win! Rock beats Scissors";
+      return "you win!";
     }
   } else if (playerChoice === "paper") {
     if (computerChoice === "scissors") {
       computerScore++;
-      return "You Lose! Scissors beats Paper";
+      return "you lose!";
     } else {
       playerScore++;
-      return "You Win! Paper beats Rock";
+      return "you win!";
     }
   } else if (playerChoice === "scissors") {
     if (computerChoice === "rock") {
       computerScore++;
-      return "You Lose! Rock beats Scissors";
+      return "you lose!";
     } else {
       playerScore++;
-      return "You Win! Scissors beats Paper";
-    }
-  }
-}
-function game() {
-  for (let i = 0; i < 5; i++) {
-    const playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
-    if (
-      playerChoice != "rock" &&
-      playerChoice != "paper" &&
-      playerChoice != "scissors"
-    ) {
-      console.log("Invalid input");
-      console.log("Round Skipped: " + rounds);
-    } else {
-      const computerChoice = getComputerChoice();
-      console.log("Player: " + playerChoice);
-      console.log("Computer: " + computerChoice);
-      console.log(playRound(playerChoice, computerChoice));
-      console.log("Round: " + rounds);
-      console.log("Player Score: " + playerScore);
-      console.log("Computer Score: " + computerScore);
+      return "you win!";
     }
   }
 }
 
-game();
+function report_result(player, comp, result) {
+  document.getElementById("results__text").innerHTML =
+    "Round: " + rounds + "<br>" + player + " vs " + comp + " - " + result;
+  document.getElementById("scores__player-score").innerHTML = playerScore;
+  document.getElementById("scores__ai-score").innerHTML = computerScore;
+  if (rounds >= 5) {
+    document.getElementById("results-final").innerHTML = display_winner();
+  } else {
+    document.getElementById("results-final").innerHTML = "";
+  }
+}
+
+function display_winner() {
+  if (playerScore > computerScore) {
+    reset();
+    return "That makes you the winner!<br>Hit a button to try again";
+  } else if (playerScore < computerScore) {
+    reset();
+    return "You LOST!?<br>Ok, let's renegotiate<br>Hit a button to try again";
+  } else {
+    reset();
+    return "It's a tie..<br>I guess we're both winners?<br>Hit a button to try again";
+  }
+}
+
+function reset() {
+  playerScore = 0;
+  computerScore = 0;
+  rounds = 0;
+}
+
+sel_rock.addEventListener("click", () => {
+  const playerChoice = "rock";
+  const c_choice = getComputerChoice();
+  report_result(playerChoice, c_choice, playRound(playerChoice, c_choice));
+});
+sel_paper.addEventListener("click", () => {
+  const playerChoice = "paper";
+  const c_choice = getComputerChoice();
+  report_result(playerChoice, c_choice, playRound(playerChoice, c_choice));
+});
+sel_scissors.addEventListener("click", () => {
+  const playerChoice = "scissors";
+  const c_choice = getComputerChoice();
+  report_result(playerChoice, c_choice, playRound(playerChoice, c_choice));
+});
